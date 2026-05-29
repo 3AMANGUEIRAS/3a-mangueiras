6ad24d4ab54c671cd0bae7dded41d7b064856496
3AMANGUEIRAS <annabatalha.moriao@gmail.com>
2026-05-29 17:28:34 -0400

    reset.js

--- api/reset.js
+++ api/reset.js
@@ -1,20 +1,14 @@
 import pg from 'pg';
 
-let db;
-function getDb() {
-  if (!db) {
-    db = new pg.Pool({
-      connectionString: "postgresql://postgres:eGfCkadCCSDaLOGYwuccpdFmSQENnaCc@autorack.proxy.rlwy.net:49211/railway",
-      ssl: { rejectUnauthorized: false },
-      max: 3
-    });
-  }
-  return db;
-}
+const client = new pg.Client({
+  connectionString: "postgresql://postgres:eGfCkadCCSDaLOGYwuccpdFmSQENnaCc@autorack.proxy.rlwy.net:49211/railway",
+  ssl: { rejectUnauthorized: false }
+});
 
 export default async function handler(req, res) {
-  const db = getDb();
-  await db.query(`DELETE FROM sessions`);
-  await db.query(`DELETE FROM leads WHERE nome ILIKE 'anna' OR telefone LIKE '%92984379847%' OR telefone LIKE '%92985094849%'`);
+  await client.connect();
+  await client.query(`DELETE FROM sessions`);
+  await client.query(`DELETE FROM leads WHERE nome ILIKE 'anna' OR nome ILIKE 'oi' OR telefone LIKE '%92984379847%' OR telefone LIKE '%92985094849%'`);
+  await client.end();
   return res.status(200).json({ ok: true, message: "Dados de teste removidos!" });
 }
